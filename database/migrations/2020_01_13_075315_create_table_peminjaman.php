0588d8c66f647ff72749e90eb38370d872af473b
ChatleaCinta <chatlea_cinta_27rpl@student.smktelkom-mlg.sch.id>
2020-01-14 14:21:54 +0700

    migration

---
+++ database/migrations/2020_01_13_075315_create_table_peminjaman.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePeminjaman extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_peminjaman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tgl');
+            $table->Integer('id_anggota');
+            $table->Integer('id_petugas');
+            $table->date('tgl_deadline');
+            $table->string('denda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_peminjaman');
+    }
+}
